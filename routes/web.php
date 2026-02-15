9e3a2740ae83bce3d4e452e507f33c79f34a7e11
jus7acolombian <95142083+jus7code@users.noreply.github.com>
2026-02-15 11:59:14 -0500

    Adición de las rutas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/perfil', function () {
+    return view('perfil');
+});
+
+Route::get('/perfil/intereses', function () {
+    return view('intereses');
+});
+
+Route::get('/perfil/habilidades', function () {
+    return view('habilidades');
+});
+
+
+Route::get('/perfil/metas', function () {
+    return view('metas');
+});
